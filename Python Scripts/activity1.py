debd352ee0cf60b3d3329b75d2973678df456df4
jwalakc <75238356+jwalakc@users.noreply.github.com>
2020-12-07 20:12:59 -0500

    Add files via upload

---
+++ Python Scripts/activity1.py
@@ -0,0 +1,4 @@
+userName = input("Enter name:")
+userAge = input("Enter age:")
+newAge = 2020 + 100 - int(userAge)
+print(userName + " will turn 100 years in the year: " + format(newAge))
---
+++ Python Scripts/hello.py
@@ -0,0 +1,3 @@
+#print ("Hello World")
+username = input("Enter username:")
+print("Username is: " + username)
