efe888ab74a808b0eaf3ee627cafc0cd0dd649b2
jwalakc <75238356+jwalakc@users.noreply.github.com>
2020-12-10 18:23:10 -0500

    Add files via upload

---
+++ Python Scripts/ClassofCars.py
@@ -0,0 +1,29 @@
+class Car:
+    'This class represents a car'
+
+    def __init__(self, manufacturer, model, make, transmission, color):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.make = make
+        self.transmission = transmission
+        self.color = color
+
+    def accelerate(self):
+        print(self.manufacturer + " " + self.model + " is moving")
+
+    def stop(self):
+        print(self.manufacturer + " " + self.model + " is stopping")
+
+car1 = Car("Toyota", "Corolla", "2015", "Manual", "White")
+car2 = Car("Maruti", "800", "2013", "Manual", "Red")
+car3 = Car("Suzuki", "Swift", "2017", "Automatic", "Black")
+
+car1.accelerate()
+car1.stop()
+print("****************************")
+car2.accelerate()
+car2.stop()
+print("****************************")
+car3.accelerate()
+car3.stop()
+print("****************************")
